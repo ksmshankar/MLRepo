03ef497e3a3a1ec3314123217511496f7e85c201
ksmshankar <kshankar14@gmail.com>
2025-01-11 00:26:47 +0530

    Add simple ML Code fot Linear Regression

---
+++ MLRepo/ml_code.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Sample data
+X = np.array([[1], [2], [3]])
+y = np.array([1, 2, 3])
+
+# Model training
+model = LinearRegression().fit(X, y)
+
+# Prediction
+print('Prediction for 4:', model.predict([[4]]))
